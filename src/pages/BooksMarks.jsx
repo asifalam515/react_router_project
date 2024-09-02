52f27608c9264f375bbe0d67bcd2329a7aa8004e
asifalam515 <asibulalam515@gmail.com>
2024-09-02 11:08:10 +0600

    local storage added

--- src/pages/BooksMarks.jsx
+++ src/pages/BooksMarks.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
+import { getBlogs } from "../utils";
+import BlogCard from "./BlogCard";
 
 const BooksMarks = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    const storedBlogs = getBlogs();
+    setBlogs(storedBlogs);
+  }, []);
   return (
-    <div>
-      <Navbar></Navbar>
-      <h1 className="text-5xl">BookMarks Page</h1>
+    <div className="grid justify-center grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {blogs.map((blog) => (
+        <BlogCard key={blog.id} blog={blog}></BlogCard>
+      ))}
     </div>
   );
 };
